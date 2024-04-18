3cccd7249461f6321e7715e613d3651fd1a97c04
José María Sosa Resendiz <jose.skedar@gmail.com>
2024-04-18 13:03:39 -0600

    explicit initializable

--- contracts/SwapToMpEthOnLineaV1.sol
+++ contracts/SwapToMpEthOnLineaV1.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IStaking} from "./interfaces/IStaking.sol";
 import {ITokenBridge} from "./interfaces/ITokenBridge.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /// @title Meta Pool Shorcut -> Swap mainnet ETH for mpETH on Linea (L2)
 
-contract SwapToMpEthOnLineaV1 is OwnableUpgradeable {
+contract SwapToMpEthOnLineaV1 is Initializable, OwnableUpgradeable {
     using SafeERC20 for IERC20;
 
     uint256 public chainId;
